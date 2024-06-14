71e791d826b76c207651e60cdb19d7e9d467cc8f
JuMoth <tkdlqjqkd@gmail.com>
2024-06-14 20:26:30 +0900

    Feat: Apply other expressions

--- obj.js
+++ obj.js
@@ -1,10 +1,10 @@
-const person = {};
-// 속성, property
-person.name = '일분이';
-person.age = 10;
-// 메서드, method
-person.introduce = function() {
-    console.log('안녕하세요. 저는 일분이이고 나이는 10살입니다.');
+const person = {
+    name: '일분이',
+    age: 10,
+    introduce: function() {
+        console.log('안녕하세요. 저는 일분이이고, 나이는 10살입니다.');
+    },
 };
 
+console.log(person.name);
 person.introduce();
