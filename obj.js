b9a17b2dd205576a74e7b259b7f92d21bb3ce88f
JuMoth <tkdlqjqkd@gmail.com>
2024-06-14 20:36:28 +0900

    Feat: Use Backtick&this

--- obj.js
+++ obj.js
@@ -2,9 +2,17 @@ const person = {
     name: '일분이',
     age: 10,
     introduce: function() {
-        console.log('안녕하세요. 저는 일분이이고, 나이는 10살입니다.');
+        console.log(`안녕하세요. 저는 ${this.name}이고, 나이는 ${this.age}살입니다.`);
     },
 };
 
-console.log(person.name);
+const person2 = {
+    name: '이분이',
+    age: 8,
+    introduce: function() {
+        console.log(`안녕하세요. 저는 ${this.name}이고, 나이는 ${this.age}살입니다.`);
+    },
+};
+
 person.introduce();
+person2.introduce();
