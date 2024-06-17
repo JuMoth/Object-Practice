fb90c453afc00dc8a7070f9170c1968fe596a408
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 16:54:53 +0900

    add inf5

--- obj2.js
+++ obj2.js
@@ -1,19 +1,18 @@
-function Card(num, color) {
+class Card {
+  constructor(num, color) {
     this.num = num;
     this.color = color;
     this.init();
+  }
+
+  init() {
+    const mainElem = document.createElement("div");
+    mainElem.style.color = this.color;
+    mainElem.innerHTML = this.num;
+    mainElem.classList.add("card");
+    document.body.appendChild(mainElem);
+  }
 }
 
-Card.prototype = {
-    constructor: Card,
-    init: function() {
-        const mainElem = document.createElement('div');
-        mainElem.style.color = this.color;
-        mainElem.innerHTML = this.num;
-        mainElem.classList.add('card');
-        document.body.appendChild(mainElem);
-    }
-};
-
-const card1 = new Card(1, 'green');
-const card2 = new Card(7, 'blue');
+const card1 = new Card(10, "green");
+const card2 = new Card(70, "blue");
