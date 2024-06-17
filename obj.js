32239564534f69275fc3123047d1328decc23b1a
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 14:10:51 +0900

    Feat: about constructor & instance

--- obj.js
+++ obj.js
@@ -6,13 +6,17 @@ const person = {
     },
 };
 
-const person2 = {
-    name: '이분이',
-    age: 8,
-    introduce: function() {
-        console.log(`안녕하세요. 저는 ${this.name}이고, 나이는 ${this.age}살입니다.`);
-    },
-};
+// 생성자 (constructor)
+function Person(nickname, age) {
+    this.nickname = nickname;
+    this.age = age;
+    this.introduce = function () {
+        console.log(`안녕하세요. 저는 ${this.nickname}이고, 나이는 ${this.age}살입니다.`);
+    }
+}
+// 인스턴스(instance)
+const person1 = new Person('일분이', 10);
+const person2 = new Person('이분이', 8);
 
-person.introduce();
+person1.introduce();
 person2.introduce();
