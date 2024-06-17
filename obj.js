72283ffe9b65f656d372ab0256f01e989df79c05
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 14:19:31 +0900

    Feat: use prototype

--- obj.js
+++ obj.js
@@ -10,10 +10,12 @@ const person = {
 function Person(nickname, age) {
     this.nickname = nickname;
     this.age = age;
-    this.introduce = function () {
-        console.log(`안녕하세요. 저는 ${this.nickname}이고, 나이는 ${this.age}살입니다.`);
-    }
 }
+
+Person.prototype.introduce = function () {
+    console.log(`안녕하세요. 저는 ${this.nickname}이고, 나이는 ${this.age}살입니다.`);
+}
+
 // 인스턴스(instance)
 const person1 = new Person('일분이', 10);
 const person2 = new Person('이분이', 8);
